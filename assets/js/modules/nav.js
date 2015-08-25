2ea3e027ddc16fe887213502527cc57a6e4730a5
Phil Smith <phil@amillionmonkeys.co.uk>
2015-08-25 16:00:42 +0100

    Initial commit

---
+++ assets/js/modules/nav.js
@@ -0,0 +1,9 @@
+var Nav = {
+	init: function() {
+		if ($('.nav-collapse').length>0) {
+		    responsiveNav(".nav-collapse");
+		}
+	}
+}
+
+
---
+++ assets/js/modules/news-widget.js
@@ -0,0 +1,22 @@
+// An example of a good JS module
+//
+// var s,
+// 	NewsWidget = {
+// 		settings: {
+// 			numArticles: 5,
+// 			articleList: $("#article-list"),
+// 		    moreButton: $("#more-button")
+// 		},
+// 		init: function(){
+// 			s = this.settings;
+// 			this.bindUIActions();
+// 		},
+// 		bindUIAdctions: function(){
+// 			s.moreButton.on("click", function(){
+// 				NewsWidget.getMoreArticles(s.numArticles);
+// 			});
+// 		},
+// 		getMoreArticles: function(numToGet) {
+			
+// 		}
+// 	}
