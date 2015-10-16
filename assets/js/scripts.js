83cf7397225f2c3839941fbe15eb4d54ebd8ddbb
Phil Smith <phil@amillionmonkeys.co.uk>
2015-10-16 19:35:22 +0100

    Add codekit includes to js file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,6 @@
+// @codekit-prepend "../bower_components/jquery/dist/jquery.min.js", "../bower_components/responsive-nav/responsive-nav.js";
+
+
 if ($('.main-nav').length>0) {
     responsiveNav(".main-nav");
 }
