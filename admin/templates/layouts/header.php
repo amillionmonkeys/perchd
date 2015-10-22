d78a1a0d2054b4dc5bdf50b8733c0799266ac10e
Phil Smith <phil@amillionmonkeys.co.uk>
2015-10-22 11:42:06 +0100

    ditch modernizr

--- admin/templates/layouts/header.php
+++ admin/templates/layouts/header.php
@@ -17,7 +17,6 @@
             h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
           })(document);
         </script>
-        <script src="/assets/bower_components/modernizr/modernizr.js"></script>
     </head>
     <body>
         <header class="main-header">
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,5 @@
-// @codekit-prepend "../bower_components/jquery/dist/jquery.min.js", "../bower_components/responsive-nav/responsive-nav.js";
+// @codekit-prepend "../bower_components/jquery/dist/jquery.min.js", "../bower_components/responsive-nav/responsive-nav.js", "modules/modernizr-custom.js";
+
 
 
 if ($('.main-nav').length>0) {
